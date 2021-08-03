43ccf62d080cecb2131980e6f6b9718b00e912ab
kk_boomer <fking.dmitry@gmail.com>
2021-08-03 15:46:46 +0200

    Добавлены шрифты и основные цвета

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static final Color background = Colors.white;
+  static final Color accent = Colors.black;
+  static final Color error = Color(0xE80000);
+  static final Color mainGrey = Color(0x939393);
+  static final Color blue = Color(0x409EFF);
+  static final Color orange = Color(0xF7941D);
+  static final Color headerLine = Color(0xCF497E);
+}
